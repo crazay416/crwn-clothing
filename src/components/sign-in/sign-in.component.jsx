cd267da11ceec51f24228ac7572f21be9b43a39f
Isaiah Guardado <gisaiah416@gmail.com>
2021-11-07 01:14:39 -0700

    User is now able to Sign in and out as well as adding it to the header

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -36,8 +36,10 @@ class SignIn extends React.Component {
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email " required />
                     <FormInput name="password" type="password" value={this.state.password} label="password" handleChange={this.handleChange} required />
 
-                    <CustomButton type="submit"> Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> {" "} Sign in with Google {" "}</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit"> Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> {" "} Sign in with Google {" "}</CustomButton>
+                    </div>
                 </form>
 
             </div>
